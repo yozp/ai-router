05d45b8abcfc66c4673360eed6b222a8d2c2e1bc
yozp <a1371361879@outlook.com>
2026-06-11 20:07:35 +0800

    后端初始化

---
+++ src/main/java/com/yzj/airouter/AiRouterApplication.java
@@ -0,0 +1,13 @@
+package com.yzj.airouter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiRouterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiRouterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yzj/airouter/AiRouterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yzj.airouter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiRouterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
