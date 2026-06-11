a5d1fb15b015fcc580ba7acecebd3de6ec04d4da
yozp <a1371361879@outlook.com>
2026-06-11 20:09:08 +0800

    后端初始化

---
+++ sql/ai_router.sql
@@ -0,0 +1,28 @@
+-- 创建库
+create database if not exists ai_router;
+
+-- 切换库
+use ai_router;
+
+-- 用户表
+create table if not exists user
+(
+    id           bigint auto_increment comment 'id' primary key,
+    userAccount  varchar(256)                           not null comment '账号',
+    userPassword varchar(512)                           not null comment '密码',
+    userName     varchar(256)                           null comment '用户昵称',
+    userAvatar   varchar(1024)                          null comment '用户头像',
+    userProfile  varchar(512)                           null comment '用户简介',
+    userRole     varchar(256) default 'user'            not null comment '用户角色：user/admin',
+    editTime     datetime     default CURRENT_TIMESTAMP not null comment '编辑时间',
+    createTime   datetime     default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime   datetime     default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete     tinyint      default 0                 not null comment '是否删除',
+    UNIQUE KEY uk_userAccount (userAccount),
+    INDEX idx_userName (userName)
+    ) comment '用户' collate = utf8mb4_unicode_ci;
+
+-- 密码是 12345678(MD5 加密 + 盐值 yupi)
+INSERT INTO user (id, userAccount, userPassword, userName, userAvatar, userProfile, userRole) VALUES
+                                                                                                  (1, 'admin', '10670d38ec32fa8102be6a37f8cb52bf', '管理员', 'https://www.codefather.cn/logo.png', '系统管理员', 'admin'),
+                                                                                                  (2, 'user', '10670d38ec32fa8102be6a37f8cb52bf', '普通用户', 'https://www.codefather.cn/logo.png', '我是一个普通用户', 'user');
--- src/main/java/com/yzj/airouter/AiRouterApplication.java
+++ src/main/java/com/yzj/airouter/AiRouterApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AiRouterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AiRouterApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AiRouterApplication.class, args);
+    }
 
 }
